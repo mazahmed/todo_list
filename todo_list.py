05d6243f235ab8718c80580c5c131d63198a029d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-26 17:59:59 -0700

    further study medium done

--- todo_list.py
+++ todo_list.py
@@ -20,6 +20,25 @@ def view_list(my_list):
     for action in my_list:
         print action
 
+def remove_first_item(my_list):
+    """delete first item in list"""
+    first = my_list[0]
+    my_list.remove(first)
+
+
+def index_add_to_list(my_list):
+    """add user's answer to specific index, also user's answer"""
+    todo = (raw_input("What would you like to add? "))
+    index = int(raw_input("Where would you like to add it? "))
+    #int(index)
+    my_list.insert(index-1, todo)
+
+def index_edit_list(my_list):
+    """edit list at given index"""
+    index = int(raw_input("What number item would you like to change? "))
+    edit = (raw_input("What would you like to change it to? "))
+    my_list[index-1] = edit
+
 
 def display_main_menu(my_list):
     """Displays main options, takes in user input, and calls view or add function."""
@@ -29,6 +48,9 @@ def display_main_menu(my_list):
     A. Add a new item
     B. View list
     C. Quit the program
+    D. Delete the first item
+    E. Insert to specific index
+    F. Change a particular list item
     >>> """
 
     while True:
@@ -41,6 +63,12 @@ def display_main_menu(my_list):
             view_list(my_list)
         elif answer == "C":
             break
+        elif answer == "D":
+            remove_first_item(my_list)
+        elif answer == "E":
+            index_add_to_list(my_list)
+        elif answer == "F":
+            index_edit_list(my_list)
         else:
             print user_options
 
